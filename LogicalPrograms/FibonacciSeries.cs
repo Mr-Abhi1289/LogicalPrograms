b07d72b940ca9885f9555de36d49ef78498ac7a6
unknown <abhimvj575@gmail.com>
2022-12-10 20:05:08 +0530

    [Abhilash] Added: finding Fibonacci Series

---
+++ LogicalPrograms/FibonacciSeries.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class FibonacciSeries
+    {
+        public void Fibonacci()
+          
+        {
+            Console.WriteLine("enter the number");
+            int num=Convert.ToInt32(Console.ReadLine());    
+            int firstNumber = 0, secondNumber = 1, thirdNumber;
+            Console.WriteLine(firstNumber);
+            
+            
+            for (int i = 1; i < num; i++)
+            {
+                thirdNumber = firstNumber + secondNumber;
+                firstNumber = secondNumber;
+                secondNumber = thirdNumber;
+                Console.WriteLine(thirdNumber);
+
+
+            }
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -6,13 +6,17 @@
         {
             while (true)
             {
-                Console.WriteLine("1.PrimeNumber");
+                Console.WriteLine("1.PrimeNumber\n2.FibonacciSeries");
                 int option =Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
                         PrimeNumber prime =new PrimeNumber();
                         prime.Prime();
+                        break;
+                    case 2:
+                        FibonacciSeries series= new FibonacciSeries();
+                        series.Fibonacci();
                         break;
                 }
 
