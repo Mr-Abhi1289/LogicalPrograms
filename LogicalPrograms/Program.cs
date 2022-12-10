d48cd46dadd790d0cd43f9953b17813663b2d818
unknown <abhimvj575@gmail.com>
2022-12-10 19:34:45 +0530

    [Abhilash] Added: checking prime numbers

---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace LogicalPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("1.PrimeNumber");
+                int option =Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        PrimeNumber prime =new PrimeNumber();
+                        prime.Prime();
+                        break;
+                }
+
+            }
+            
+        }
+    }
+}
